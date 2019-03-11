0f4b932f9ff60bf6a816c27f26b06a2a468fa441
NanoDano <nanodano@devdungeon.com>
2019-03-10 23:30:37 -0500

    Work in progress, working HTTP request

--- App.js
+++ App.js
@@ -10,38 +10,66 @@ import {StyleSheet, Text, View, Button, AsyncStorage} from 'react-native';
 
 export default class App extends React.Component {
 
+
     constructor(props) {
         super(props);
+        this.sessionId = "l262ugnnqxb697uirehsml6aruh0lbug";
         this.saveGps = this.saveGps.bind(this);
-        this.asyncStorageKey = "gps-bookmarker-data"
+        // this.asyncStorageKey = "gps-bookmarker-data"
     }
 
     async wipeBookmarks() {
         console.log('Wiping bookmarks.');
-        await AsyncStorage.setItem(this.asyncStorageKey, "");
+
+        // TODO sendHttpPostToRestApi(delete all bookmarks)
+        // await AsyncStorage.setItem(this.asyncStorageKey, "");
+    }
+
+
+    createNewBookmark() {
+
+    }
+
+
+    getBookmarkList() {
+        let url = "http://192.168.1.156:8000/api/bookmarks.json";
+        // let url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json';
+
+
+        let response = fetch(url, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                //'Content-Type': 'application/json',
+                Cookie: 'sessionid=' + this.sessionId
+            }
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                return responseJson;
+            })
+            .catch((error) => {
+                console.log(error);
+                throw(error);
+            });
     }
 
     saveGps() {
         navigator.geolocation.getCurrentPosition(async (loc) => {
-                let data = await AsyncStorage.getItem(this.asyncStorageKey, () => {
-                });
-                try {
-                    if (data === null) {
-                        data = Array();
-                    } else {
-                        data = JSON.parse(data);
-                    }
-                    data.push(loc);
-                    await AsyncStorage.setItem(this.asyncStorageKey, JSON.stringify(data));
-                } catch (err) {
-                    console.log("Error setting bookmark data..." + err)
-                }
+                console.log('Storing location...');
+                // TODO sendHttpPostToRstApi(loc)
+
+                this.getBookmarkList()
+
+
             },
             (err) => {
                 console.log('Error pulling GPS coordinates.');
-                console.log(err)
+                console.log(err);
+                throw(err);
             },
-            {enableHighAccuracy: true})
+            {enableHighAccuracy: true});
     }
 
     render() {
@@ -52,6 +80,8 @@ export default class App extends React.Component {
             </View>
         );
     }
+
+
 }
 
 const
