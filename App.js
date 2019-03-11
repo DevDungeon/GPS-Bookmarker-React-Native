034c4fa7fc589b5bad94ca9e00bc9f3150bc700a
NanoDano <nanodano@devdungeon.com>
2019-03-11 01:56:10 -0500

    Working login/logout and session storage for API requests

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button, AsyncStorage} from 'react-native';
+import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
 
 // TODO Store session securely with https://github.com/oblador/react-native-keychain
 // TODO Add ListView/ScrollView to review all created bookmarks
@@ -8,96 +9,88 @@ import {StyleSheet, Text, View, Button, AsyncStorage} from 'react-native';
 // TODO Button "View bookmarks" -> Have a ScrollView with a List and all bookmarks+descriptions
 // TODO Give feedback on save/print errors
 // TODO Allow 'email bookmarks'
+// TODO Create a new tab with a list of all the current bookmarks
+
+let baseUrl = "http://192.168.1.156:8000/api/";
 
 
-export default class App extends React.Component {
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 
+
+class MainScreen extends React.Component {
 
     constructor(props) {
         super(props);
-
-        // TODO Add login and store session
-        this.sessionId = "l2d2ugnnqxb69ddddirehsml6aruh0lbug";
-
-        this.saveGps = this.saveGps.bind(this);
+        this.saveBookmark = this.saveBookmark.bind(this);
         this.listBookmarks = this.listBookmarks.bind(this);
-        // this.asyncStorageKey = "gps-bookmarker-data"
-        this.state = {'currentMessage': 'GPS coordinate not yet saved.',
-        'currentUsername': 'N/A'}
+        this.state = {'currentMessage': 'GPS coordinate not yet saved.'}
     }
 
-
     createNewBookmark(loc) {
-        let url = "http://192.168.1.156:8000/api/bookmarks.json";
-        // let url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json';
+        let url = baseUrl + "bookmarks.json";
 
-        console.log(loc);
-        let response = fetch(url, {
+        fetch(url, {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
-                Cookie: 'sessionid=' + this.sessionId,
             },
             body: JSON.stringify({
                 lat: loc.coords.latitude,
                 lon: loc.coords.longitude,
                 alt: loc.coords.altitude
-            })
-        })
-            .then((response) => {
-                if (response.status === 403) {
-                    console.log('Authentication error...');
-                    this.setState({currentMessage: 'Authentication error.'});
-                }
-                return response.json()
-            })
-            .then((responseJson) => {
-                console.log(responseJson);
+            }),
+            credentials: 'same-origin',
+        }).then((response) => {
+            if (response.status === 403) {
+                console.log('Authentication error...');
+                this.setState({currentMessage: 'Authentication error.'});
+            }
+            return response.json()
+        }).then((responseJson) => {
+            console.log(responseJson);
 
-                return responseJson;
-            })
-            .catch((error) => {
-                let message = 'Error saving new bookmark.';
-                this.setState({'currentMessage': message + "\n" + error});
-                console.log(error);
-                //throw(error);
-            });
+            return responseJson;
+        }).catch((error) => {
+            let message = 'Error saving new bookmark.';
+            this.setState({'currentMessage': message + "\n" + error});
+            console.log(error);
+        });
     }
 
-
     getBookmarkList() {
-        let url = "http://192.168.1.156:8000/api/bookmarks.json";
-        // let url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json';
+        let url = baseUrl + "bookmarks.json";
 
-        let response = fetch(url, {
+        fetch(url, {
             method: 'GET',
             headers: {
                 Accept: 'application/json',
-                //'Content-Type': 'application/json',
-                Cookie: 'sessionid=' + this.sessionId
+            },
+            credentials: 'same-origin'
+        }).then((response) => {
+            if (response.status === 403) {
+                console.log('Authentication error...');
+                this.setState({currentMessage: 'Authentication error.'});
             }
-        })
-            .then((response) => {
-                if (response.status === 403) {
-                    console.log('Authentication error...');
-                    this.setState({currentMessage: 'Authentication error.'});
-                }
-                return response.json();
-            })
-            .then((responseJson) => {
-                console.log(responseJson);
-                return responseJson;
-            })
-            .catch((error) => {
-                console.log(error);
-                throw(error);
-            });
+            return response.json();
+        }).then((responseJson) => {
+            console.log(responseJson);
+            return responseJson;
+        }).catch((error) => {
+            console.log(error);
+            throw(error);
+        });
     }
 
-    saveGps() {
+    saveBookmark() {
         navigator.geolocation.getCurrentPosition(async (loc) => {
-
                 this.setState({'currentMessage': 'Saving location...'});
                 console.log('Storing location...');
                 this.createNewBookmark(loc);
@@ -106,7 +99,6 @@ export default class App extends React.Component {
             (err) => {
                 let message = 'Error pulling GPS coordinates.';
                 this.setState({'currentMessage': message});
-                console.log(message);
                 console.log(err);
                 throw(err);
             },
@@ -122,20 +114,94 @@ export default class App extends React.Component {
         return (
             <View style={styles.container}>
                 <Text>{this.state.currentMessage}</Text>
-                <Button title="Save GPS location" onPress={this.saveGps}/>
+                <Button title="Save GPS location" onPress={this.saveBookmark}/>
                 <Button title="List all bookmarks" onPress={this.listBookmarks}/>
-                <Text>Logged in as: {this.state.currentUsername}</Text>
             </View>
         );
     }
 }
 
-const
-    styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-    });
+
+class LoginScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUsername: '',
+            password: '',
+            currentMessage: 'Not logged in.'
+        };
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
+    }
+
+    logout() {
+        fetch(baseUrl + 'logout', {
+            method: 'GET',
+            credentials: 'same-origin',
+        }).then((response) => {
+            this.setState({currentMessage: 'Now logged out.'});
+            console.log(response);
+        }).catch((error) => {
+            console.log('Error logging out.');
+            console.log(error);
+            this.setState({currentMessage: 'Error logging out...'})
+        })
+    }
+
+
+    login() {
+        this.setState(prevState => ({
+            currentMessage: 'Logging in...'
+        }));
+        console.log('STate:::');
+        console.log(this.state);
+
+        let formData = new FormData();
+        formData.append('username', this.state.currentUsername);
+        formData.append('password', this.state.password);
+        let url = baseUrl + 'login/';
+
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+            credentials: 'same-origin',
+        }).then((response) => {
+            if (response.status === 200) {
+                this.setState({currentMessage: 'Now logged in'});
+                console.log(response);
+            } else {
+                throw("Error logging in.")
+            }
+        }).catch((error) => {
+            console.log(error);
+            this.setState({currentMessage: 'Error logging in...Check credentials'})
+        })
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text>Enter Username:</Text>
+                <TextInput
+                    onChangeText={(text) => this.setState({currentUsername: text})}>
+                    {this.state.currentUsername}
+                </TextInput>
+                <Text>Enter Password:</Text>
+                <TextInput
+                    onChangeText={(text) => this.setState({password: text})}>
+                    {this.state.password}
+                </TextInput>
+                <Button onPress={this.login} title="Login"/>
+                <Text>{this.state.currentMessage}</Text>
+                <Button onPress={this.logout} title="Logout"/>
+            </View>
+        );
+    }
+}
+
+const TabNavigator = createBottomTabNavigator({
+    Bookmark: MainScreen,
+    Login: LoginScreen,
+});
+
+export default createAppContainer(TabNavigator);
