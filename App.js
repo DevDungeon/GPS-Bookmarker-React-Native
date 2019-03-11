878c346fb4a714ee71c21659dde46d14eafb0975
NanoDano <nanodano@devdungeon.com>
2019-03-11 00:29:33 -0500

    Barely working API operations over HTTP

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {StyleSheet, Text, View, Button, AsyncStorage} from 'react-native';
 
+// TODO Store session securely with https://github.com/oblador/react-native-keychain
+// TODO Add ListView/ScrollView to review all created bookmarks
 // TODO add a "Description" field to store with each location
 // TODO Show the current location on screen before committing the save
 // TODO Button "View bookmarks" -> Have a ScrollView with a List and all bookmarks+descriptions
@@ -13,28 +15,60 @@ export default class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.sessionId = "l262ugnnqxb697uirehsml6aruh0lbug";
+
+        // TODO Add login and store session
+        this.sessionId = "l2d2ugnnqxb69ddddirehsml6aruh0lbug";
+
         this.saveGps = this.saveGps.bind(this);
+        this.listBookmarks = this.listBookmarks.bind(this);
         // this.asyncStorageKey = "gps-bookmarker-data"
-    }
-
-    async wipeBookmarks() {
-        console.log('Wiping bookmarks.');
-
-        // TODO sendHttpPostToRestApi(delete all bookmarks)
-        // await AsyncStorage.setItem(this.asyncStorageKey, "");
+        this.state = {'currentMessage': 'GPS coordinate not yet saved.',
+        'currentUsername': 'N/A'}
     }
 
 
-    createNewBookmark() {
+    createNewBookmark(loc) {
+        let url = "http://192.168.1.156:8000/api/bookmarks.json";
+        // let url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json';
 
+        console.log(loc);
+        let response = fetch(url, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Cookie: 'sessionid=' + this.sessionId,
+            },
+            body: JSON.stringify({
+                lat: loc.coords.latitude,
+                lon: loc.coords.longitude,
+                alt: loc.coords.altitude
+            })
+        })
+            .then((response) => {
+                if (response.status === 403) {
+                    console.log('Authentication error...');
+                    this.setState({currentMessage: 'Authentication error.'});
+                }
+                return response.json()
+            })
+            .then((responseJson) => {
+                console.log(responseJson);
+
+                return responseJson;
+            })
+            .catch((error) => {
+                let message = 'Error saving new bookmark.';
+                this.setState({'currentMessage': message + "\n" + error});
+                console.log(error);
+                //throw(error);
+            });
     }
 
 
     getBookmarkList() {
         let url = "http://192.168.1.156:8000/api/bookmarks.json";
         // let url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json';
-
 
         let response = fetch(url, {
             method: 'GET',
@@ -44,7 +78,13 @@ export default class App extends React.Component {
                 Cookie: 'sessionid=' + this.sessionId
             }
         })
-            .then((response) => response.json())
+            .then((response) => {
+                if (response.status === 403) {
+                    console.log('Authentication error...');
+                    this.setState({currentMessage: 'Authentication error.'});
+                }
+                return response.json();
+            })
             .then((responseJson) => {
                 console.log(responseJson);
                 return responseJson;
@@ -57,31 +97,37 @@ export default class App extends React.Component {
 
     saveGps() {
         navigator.geolocation.getCurrentPosition(async (loc) => {
+
+                this.setState({'currentMessage': 'Saving location...'});
                 console.log('Storing location...');
-                // TODO sendHttpPostToRstApi(loc)
-
-                this.getBookmarkList()
-
-
+                this.createNewBookmark(loc);
+                this.setState({'currentMessage': 'Done saving location.'});
             },
             (err) => {
-                console.log('Error pulling GPS coordinates.');
+                let message = 'Error pulling GPS coordinates.';
+                this.setState({'currentMessage': message});
+                console.log(message);
                 console.log(err);
                 throw(err);
             },
             {enableHighAccuracy: true});
     }
 
+    listBookmarks() {
+        let data = this.getBookmarkList();
+        console.log(data);
+    }
+
     render() {
         return (
             <View style={styles.container}>
+                <Text>{this.state.currentMessage}</Text>
                 <Button title="Save GPS location" onPress={this.saveGps}/>
-                <Button title="Wipe all bookmarks" onPress={this.wipeBookmarks}/>
+                <Button title="List all bookmarks" onPress={this.listBookmarks}/>
+                <Text>Logged in as: {this.state.currentUsername}</Text>
             </View>
         );
     }
-
-
 }
 
 const
--- README.rst
+++ README.rst
@@ -9,5 +9,6 @@ Created using ``expo init``.
 
 Build using ``expo build:android``
 
-Live stream of coding: https://www.youtube.com/watch?v=PwwXIs32JG4
-
+Live stream of coding:
+- https://www.youtube.com/watch?v=PwwXIs32JG4
+- https://www.youtube.com/watch?v=-eOQGDG_LxY
